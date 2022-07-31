81ef4d58532c4f1cabe99593d5bb74db34ed57e6
Mark Yip <yip279@gmail.com>
2022-07-31 19:35:07 +0800

    Initialize leaflet instance without error.

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,26 @@
+import { useEffect, useRef } from "react";
+
 export default function Home() {
-  return <h1>Hello World</h1>;
+  const mapSideEffectRef = useRef();
+
+  useEffect(() => {
+    const promiseShouldBeWaited = mapSideEffectRef.current ?? Promise.resolve();
+    const mapInstancePromise = promiseShouldBeWaited
+      .then(() => import("leaflet"))
+      .then(({ map }) => {
+        return map("map");
+      });
+
+    return () =>
+      (mapSideEffectRef.current = mapInstancePromise.then((mapInstance) => {
+        mapInstance.remove();
+      }));
+  });
+
+  return (
+    <>
+      <h1>Leaflet Demo</h1>
+      <div id="map" className="h-48"></div>
+    </>
+  );
 }
